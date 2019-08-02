95e2bfe56f9012c3cc0b20f4ff2a3e1de1fe87dc
John Karrell Speck <speckworks@gmail.com>
2019-08-02 15:35:28 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+first_number = a
+second_number = b
+second_number != 0
+sum = a + b
+product = a * b
+difference = a - b
+quotient = a/b
+a = gets.chomp
+b = gets.chomp
+puts sum
+puts product
+puts difference
+puts quotient
+end
